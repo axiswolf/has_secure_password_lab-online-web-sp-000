d5bbb7bebfd6a2badb89df021dff971e3718a4ea
Karen V <karenvo@usc.edu>
2019-12-12 03:24:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     # rejects invalid passwords
     # rejects empty passwords
     @user = User.find_by(name: params[:user][:name])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
   end
 
