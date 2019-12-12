c19d456d39f2d9f0e491eee727af7a134e26ad8c
Karen V <karenvo@usc.edu>
2019-12-12 02:56:44 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
-class User
+class User < ActiveRecord::Base
 
+  def password = (new_password)
+    self.password_digest = dumb_hash(new_password)
+  end
+
+  def authenticate(password)
+    return nil unless dumb_hash(password) == password_digest
+  end
+
+  private
+
+  def dumb_hash(input)
+    input.bytes.reduce(:+)
+  end
 
 end
