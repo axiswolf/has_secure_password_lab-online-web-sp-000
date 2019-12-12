2611e3d793d65fc4a9349740979e60b5f708f9ce
Karen V <karenvo@usc.edu>
2019-12-12 03:12:29 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,5 @@
-gem 'bcrypt'
+
 class User < ActiveRecord::Base
   has_secure_password
-
-
-  def password=(new_password)
-    self.password_digest = password_confirmation
-  end
-
-  # def authenticate(password)
-  #   return nil unless dumb_hash(password) == password_digest
-  # end
-
-  # private
-  #
-  # def dumb_hash(input)
-  #   input.bytes.reduce(:+)
-  # end
 
 end
