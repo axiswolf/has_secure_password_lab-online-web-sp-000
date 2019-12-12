ca44e61870eca0f5a24083c895869f2ba65d7a00
Karen V <karenvo@usc.edu>
2019-12-12 03:33:52 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     # logs you in
     # sets your password if confirmation matches
     # redirects if password and confirmation don't match
-    @user = User.create(user_params)
+    @user = User.new(user_params)
 
     if params[:user][:password] != params[:user][:password_confirmation]
       redirect_to "/signup"
