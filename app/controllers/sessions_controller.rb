592e3f9c52640c8b515f2f962ba1179c4eb4f178
Karen V <karenvo@usc.edu>
2019-12-12 03:17:54 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,9 @@ class SessionsController < ApplicationController
     # logs you with the correct password
     # rejects invalid passwords
     # rejects empty passwords
+    @user = User.find_by(name: params[:name])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
   end
 
 end
