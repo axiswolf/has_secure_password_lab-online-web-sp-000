55cc53ec4faff6b77a166dd52fdd001dca434a16
Karen V <karenvo@usc.edu>
2019-12-12 03:40:19 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,12 @@ class UsersController < ApplicationController
     #   @user.save
     #   session[:user_id] = @user.id
     # end
-    if params[:user][:password] == params[:user][:password_confirmation]
-      @user = User.create(user_params)
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      redirect_to new_user_path, alert: "Invalid password."
-    end
+    user = User.create(user_params)
+      if user.password == user.password_confirmation
+      session[:user_id] = user.id
+      else
+        redirect_to '/users/new'
+      end
   end
 
   private
