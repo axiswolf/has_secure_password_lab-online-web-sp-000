71802699d202d13f44492e63d67e7618ba17843e
Karen V <karenvo@usc.edu>
2019-12-12 03:36:21 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,21 @@ class UsersController < ApplicationController
     # logs you in
     # sets your password if confirmation matches
     # redirects if password and confirmation don't match
-    @user = User.create(user_params)
+    # @user = User.create(user_params)
+    # 
+    # if params[:user][:password] != params[:user][:password_confirmation]
+    #   redirect_to "/signup"
+    # else
+    #   @user.save
+    #   session[:user_id] = @user.id
+    # end
 
-    if params[:user][:password] != params[:user][:password_confirmation]
-      redirect_to "/signup"
-    else
-      @user.save
-      session[:user_id] = @user.id
-    end
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+    session[:user_id] = user.id
+    @user = user
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   private
